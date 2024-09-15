a9c3e441a536f493587a0643d6e930b2a218c646
这名字牛 <renkelin00@163.com>
2024-09-15 16:14:04 +0900

    feat: 前端全局状态管理

--- interview-hub-frontend/src/app/layout.tsx
+++ interview-hub-frontend/src/app/layout.tsx
@@ -1,6 +1,47 @@
+"use client";
 import BasicLayout from "@/app/layouts/BasicLayout";
-import {AntdRegistry} from "@ant-design/nextjs-registry";
-import React from "react";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import React, { useCallback, useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
+import store, { AppDispatch } from "@/stores";
+import "./globals.css";
+import { getLoginUserUsingGet } from "@/api/userController";
+import { setLoginUser } from "@/stores/loginUser";
+
+/**
+ * 全局初始化逻辑
+ * @param children
+ * @constructor
+ */
+const InitLayout: React.FC<
+  Readonly<{
+    children: React.ReactNode;
+  }>
+> = ({ children }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  // 初始化全局用户状态
+  const doInitLoginUser = useCallback(async () => {
+    const res = await getLoginUserUsingGet();
+    if (res.data) {
+      // 更新全局用户状态
+    } else {
+      setTimeout(() => {
+        const testUser = {
+          userName: "测试登录",
+          id: 1,
+          userAvatar: "https://kolin-blog.oss-cn-shanghai.aliyuncs.com/blog/202409090007739.png",
+        };
+        dispatch(setLoginUser(testUser));
+      }, 3000);
+    }
+  }, []);
+
+  // 只执行一次
+  useEffect(() => {
+    doInitLoginUser();
+  }, []);
+  return children;
+};
 
 export default function RootLayout({
   children,
@@ -10,9 +51,12 @@ export default function RootLayout({
   return (
     <html lang="zh">
       <body>
-      {/* eslint-disable-next-line react/jsx-no-undef */}
         <AntdRegistry>
-          <BasicLayout>{children}</BasicLayout>
+          <Provider store={store}>
+            <InitLayout>
+              <BasicLayout>{children}</BasicLayout>
+            </InitLayout>
+          </Provider>
         </AntdRegistry>
       </body>
     </html>
